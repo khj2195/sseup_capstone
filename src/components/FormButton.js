119d9f7b80fee2c4d7186b69d0a9cca671d88242
aquinas125 <quin125@naver.com>
2021-03-30 23:05:38 +0900

    design

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import {Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
 
@@ -31,4 +32,39 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontFamily: 'Lato-Regular',
   },
+=======
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+
+const windowHeight = Dimensions.get('window').height;
+
+const FormButton = ({buttonTitle, ...rest}) => {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} {...rest}>
+      <Text style={styles.buttonText}>{buttonTitle}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default FormButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    backgroundColor: '#2e64e5',
+    // backgroundColor: 'steelblue',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    // fontFamily: 'Lato-Regular',
+  },
+>>>>>>> bbbff20 (design)
 });
